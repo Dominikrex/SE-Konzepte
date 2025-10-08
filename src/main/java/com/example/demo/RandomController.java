bbb8ef08836ba410b4e47942dbcfab863611c74e
Dominikrex <dominikreiner@gmx.de>
2025-10-08 17:02:52 +0200

    Aufgaben 3. Tag
    Aufgaben 3. Tag

---
+++ src/main/java/com/example/demo/RandomController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController
+public class RandomController implements RandomControllerApi {
+
+    @Override
+    public ResponseEntity<Integer> getRandomNumber() {
+        int value = ThreadLocalRandom.current().nextInt();
+        return ResponseEntity.ok(value);
+    }
+}
